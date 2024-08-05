37a4220b7e4abbe3af1b8457b3445fd571f58c44
pmunge <munge.pw620@gmail.com>
2024-08-05 14:53:33 +0300

    my project

---
+++ awesome/awesome_web/users/urls.py
@@ -0,0 +1,9 @@
+# users/urls.py
+
+from django.urls import path,include
+from .import views
+
+urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
+    path("", views.dashboard, name="dashboard"),
+]
---
+++ awesome/awesome_web/users/views.py
@@ -0,0 +1,5 @@
+# Create your views here.
+from django.shortcuts import render
+
+def dashboard(request):
+    return render(request, "users/dashboard.html")
